df9360496ae5202d8b43008eef1ccb4d81691a05
Troy Martin <beef.erikson.studios@gmail.com>
2019-07-23 05:24:54 -0400

    Fancy credits added to Chapter 2

---
+++ Chapter2/fancy_credits.py
@@ -0,0 +1,14 @@
+# Fancy Credits
+# Demonstrates escape sequences
+
+
+print("\n\t\t\tFancy Credits")
+print("\t\t\t\\ \\ \\ \\ \\ \\ \\")
+print("\t\t\t\tby")
+print("\t\t\tTroy Martin")
+print("\t\t\t\\ \\ \\ \\ \\ \\ \\")
+
+print("\nSpecial thanks goes out to:")
+print("My beautiful muse Jackie, who thinks my wardrobe is fueled by an acid trip.")
+
+input("\n\nPress the Enter key to exit.")
